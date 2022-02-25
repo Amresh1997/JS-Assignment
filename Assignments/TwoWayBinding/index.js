bbb6b5f213d5061a934387b56eb6819c8aa71ccd
Amresh Kumar <77493243+Amresh1997@users.noreply.github.com>
2022-02-25 16:41:18 +0530

    Assignment update

--- Assignments/TwoWayBinding/index.js
+++ Assignments/TwoWayBinding/index.js
@@ -1,27 +1,25 @@
-(function() {
-    var element = document.querySelector('[data-twb]'),
-        scope = {};
-                  
-         var propToBind = element.getAttribute('data-twb');
-            
-         element.onkeyup = function(){
-                scope[propToBind] = element.value;
-                addScopeProperty(propToBind);
-            }
-        
-  
-        
-        function addScopeProperty(prop){
-            document.getElementById("name").innerHTML=scope[prop];
-          
-        }
-    
-   
-    changeNameByCode = function() {
-        scope.name = 'name Changed by Code';
-        document.querySelector('[data-twb]').value=scope.name;
-        document.getElementById("name").innerHTML=scope.name;
+var num={};
+
+Object.defineProperty(num,"number",
+{
+    get: function()
+    {
+        return value;
+
+    },
+    set: function(inpval)
+    {
+document.getElementById("userInput").value=inpval;
+value=inpval;
     }
-  
-   
-  })();
+});
+
+setInterval(()=>{
+num.number=Math.floor(Math.random()*100);
+},7000);
+
+function changeInput(event)
+{
+    num.number=event.target.value;
+    alert("Typed value is: "+ num.number);
+}
